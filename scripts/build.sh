93fb4653f7e3e832c8a0a0f612493fe8133cc0a6
VsevolodX <vsevolodbiryukov@icloud.com>
2026-06-05 13:46:22 -0700

    tmp: ax from commit

--- scripts/build.sh
+++ scripts/build.sh
@@ -36,7 +36,7 @@ pip list
 if [[ -n ${UPDATE_CONTENT} ]]; then
     mkdir -p ${TMP_DIR} && cd ${TMP_DIR} || exit 1
     REPO_NAME="api-examples"
-    BRANCH_NAME="main"
+    BRANCH_NAME="chore/SOF-7908"
 
     # Always clone fresh to avoid stale cached state
     rm -rf "${REPO_NAME}"
